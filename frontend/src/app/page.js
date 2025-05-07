73c2747aeaf6fc724a2d689c67f58e787da3dcd2
ominjoo <o.minjoo14@gmail.com>
2025-05-07 13:23:52 -0700

    topbar nonresponsive

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,10 +1,24 @@
 import Image from "next/image";
 import Navbar from "../components/Navbar";
+import TopBar from "../components/TopBar";
 
 export default function Home() {
+  // eventually change to fetch from backend
+  let forecasts = [
+    { day: 'Mon', value: 70 },
+    { day: 'Tue', value: 60 },
+    { day: 'Wed', value: 65 },
+    { day: 'Thu', value: 75 },
+    { day: 'Fri', value: 80 },
+    { day: 'Sat', value: 90 },
+    { day: 'Sun', value: 85 }
+  ]
   return (
     <main>
       <Navbar />
+      <div className="flex items-center justify-center min-h-screen">
+        <TopBar forecasts={forecasts} />
+    </div>
     </main>
   );
 }
