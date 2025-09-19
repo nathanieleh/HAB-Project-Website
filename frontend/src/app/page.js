d5e2810a4a716bdd70a5e0764832b03888689fc2
nathanieleh <nathanieleh@gmail.com>
2025-09-18 21:56:09 -0700

    updated about section

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -128,8 +128,8 @@ export default async function Home() {
       <div className = "flex flex-col justify-center items-center">
         <h1 className="text-2xl text-center mt-[5vh]  text-secondary font-bold " >About</h1>
         <p className="text-center text-white opacity-80 mt-[5vh] mb-[5vh] max-w-2xl leading-loose">
-          This project is a web application that provides a 7-day forecast of bioluminescence levels at Scripps Pier, La Jolla. The forecasts are based on data collected from various sources and are displayed in an easy-to-read format.
-          The application features a top bar that shows the forecast for each day, a detailed view of today's forecast, and a graph that visualizes the data over the week. The goal of this project is to help users plan their visits to Scripps Pier based on the bioluminescence levels.
+          This project is a web application that provides a 3 week forecast of bioluminescence levels at Scripps Pier, La Jolla. The forecasts are based on data collected from various sources and are displayed in an easy-to-read format.
+          The application features a top bar that shows the forecast for each week, a focused view of this week's forecast, and a graph that visualizes the data over the 3 weeks. The goal of this project is to help users plan their visits to Scripps Pier based on the bioluminescence levels.
           
           The application is built using Next.js and Tailwind CSS, providing a responsive and user-friendly interface. The data is fetched from a backend server and displayed in real-time, ensuring that users have the most up-to-date information.
           The project is still in development, and future updates will include more features and improvements to the user experience. We welcome feedback and suggestions from users to help us make this application even better.  
